b8995049abac10b66da85d03211379e5f34c5025
Tiago Vargas <tiago.ce10@outlook.com>
2023-06-08 13:32:21 -0300

    Add `Lox.Tests` mstest and its references

---
+++ Lox.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lox.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
